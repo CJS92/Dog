77a0ca5649264f9846fa2fd2fe8b4311499e051f
Cody Sloan <46944830+CJS92@users.noreply.github.com>
2019-02-15 18:44:17 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// This is a project for IT 2040.
+// Cody Sloan
+// 02/15/2019
+
+using System;
+
+public enum Gender
+{
+    Male,
+    Female
+}
+
+namespace Dog
+{
+    public class Dog
+    {
+        public string name;
+        public string owner;
+        public int age;
+        readonly Gender gender;
+
+        public Dog(string dogName, string dogOwner, int dogAge, Gender dogGender)
+        {
+            name = dogName;
+            owner = dogOwner;
+            age = dogAge;
+            gender = dogGender;
+        }
+
+        public Dog()
+        {
+        }
+
+
+        public void Bark(int to)
+        {
+            int number = 0;
+
+            while (number < 5)
+            {
+                Console.WriteLine(number);
+                number = number + 1;
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class MainClass
+        {
+            public static void Main(string[] args)
+            {
+            var dog = new Dog();
+
+            dog.Bark = "{0}";
+
+            Console.ReadLine();
+        }
+        }
+    }
